f653627ae31f590772d0344289bc9f27ec34ee11
SannanZahid <sannanzahiddev@gmail.com>
2024-11-14 18:31:28 +0400

    Implemented Card Scaling Functionality
    • Card are scaled to fit the screen container anchored to canvas which will fit according to screen device when spawning cards on game board at start.

--- Assets/Scripts/GameBoard/GameBoard.cs
+++ Assets/Scripts/GameBoard/GameBoard.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <Class-Purpose>  
 /// Class for managing state of cards on board as well as checking cards interation
@@ -15,6 +16,7 @@ public class GameBoard : MonoBehaviour
     [Space(10)]
     [SerializeField] private Transform _cardPrefab = default;
     [SerializeField] private Transform _boardWidgetHolder = default;
+    [SerializeField] private float _cellSpacing = 5f;
 
     private List<Transform> _spawnCards = new List<Transform>();
     private Transform _tempCard = default;
@@ -22,6 +24,7 @@ public class GameBoard : MonoBehaviour
     /// Takes face card sprites and pass it to card creation  
     public void SetBoard(List<Sprite> selectedCardFace)
     {
+        ScaleCardToFitContainor(selectedCardFace[0], (float)selectedCardFace.Count / 2);
         for (int i = 0; i < selectedCardFace.Count; i++)
         {
             CreateCard(i, selectedCardFace[i]);
@@ -37,6 +40,7 @@ public class GameBoard : MonoBehaviour
         foreach (Transform card in _spawnCards)
         {
             card.SetParent(_boardWidgetHolder);
+            card.GetComponent<RectTransform>().localScale = Vector3.one;
         }
     }
 
@@ -46,5 +50,34 @@ public class GameBoard : MonoBehaviour
         _tempCard.gameObject.SetActive(true);
         _tempCard.GetComponent<Card>().Init(id, cardFront);
         _spawnCards.Add(_tempCard);
+    }
+
+    //scalling cards according to containor widget
+    private void ScaleCardToFitContainor(Sprite referenceCardSize, float gridSize)
+    {
+        float containorWidth;
+        float containorHeight;
+        GridLayoutGroup boardContainor = _boardWidgetHolder.GetComponent<GridLayoutGroup>();
+        containorWidth = _boardWidgetHolder.GetComponent<RectTransform>().rect.width;
+        containorHeight = _boardWidgetHolder.GetComponent<RectTransform>().rect.height;
+        float spriteWidth = referenceCardSize.rect.width;
+        float spriteHeight = referenceCardSize.rect.height;
+        float forcastCellWidth = (containorWidth - (_cellSpacing * gridSize)) / gridSize;
+        float forcastCellHeight = (containorHeight - (_cellSpacing * gridSize)) / gridSize;
+        float ratioWidth = forcastCellWidth / spriteWidth;
+        float ratioHeight = forcastCellHeight / spriteHeight;
+
+        if (ratioHeight > 1 && ratioWidth > 1)
+        {
+            boardContainor.cellSize = new Vector2(spriteWidth, spriteHeight);
+        }
+        else if (ratioWidth < ratioHeight)
+        {
+            boardContainor.cellSize = new Vector2(spriteWidth * ratioWidth, spriteHeight * ratioWidth);
+        }
+        else
+        {
+            boardContainor.cellSize = new Vector2(spriteWidth * ratioHeight, spriteHeight * ratioHeight);
+        }
     }
 }
